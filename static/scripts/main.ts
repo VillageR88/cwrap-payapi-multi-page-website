9f8d03b4f819ee2d604fc93462eceaee64d13c78
VillageR88 <villager88pl@gmail.com>
2025-02-07 17:40:15 +0100

    Initially finished
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- static/scripts/main.ts
+++ static/scripts/main.ts
@@ -1,7 +1,16 @@
 const mobileMenuButton = document.getElementById(
 	"mobile-menu-button",
 ) as HTMLButtonElement;
+const mobileSchedule = document.getElementById(
+	"mobile-schedule",
+) as HTMLAnchorElement;
+const email2 = document.getElementById("email2") as HTMLInputElement;
 
 mobileMenuButton.addEventListener("click", () =>
 	mobileMenuButton.classList.toggle("open"),
 );
+
+mobileSchedule.addEventListener("click", () => {
+	mobileMenuButton.classList.toggle("open");
+	email2.scrollIntoView();
+});
